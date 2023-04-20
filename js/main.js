1659fe88e29a66a4f80bc46195e734b5743873ad
stieludv <19951864+stieludv@users.noreply.github.com>
2023-04-21 00:41:35 +0200

    Implementing custom validation with JS using Constraint Validation API

---
+++ js/main.js
@@ -0,0 +1,72 @@
+const form = document.querySelector(".sign-up-form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (form.checkValidity() === false) {
+        // Get inputs
+        const firstNameInput = document.getElementById("first-name");
+        const lastNameInput = document.getElementById("last-name");
+        const emailInput = document.getElementById("email");
+        const phoneInput = document.getElementById("phone");
+        const passwordInput = document.getElementById("password");
+        const passwordConfirmationInput = document.getElementById("password-confirmation");
+
+        // Validate name & lastname inputs
+        if (firstNameInput.validity.valueMissing) {
+            firstNameInput.setCustomValidity("Please enter your first name");
+        } else {
+            firstNameInput.setCustomValidity("");
+        }
+        if (lastNameInput.validity.valueMissing) {
+            lastNameInput.setCustomValidity("Please enter your last name");
+        }
+        else {
+            lastNameInput.setCustomValidity("");
+        }
+
+        // Validate email and phone inputs
+        if (emailInput.validity.valueMissing) {
+            emailInput.setCustomValidity("Please enter your email address");
+        } else if (emailInput.validity.typeMismatch) {
+            emailInput.setCustomValidity("Please enter a valid email address");
+        } else {
+            emailInput.setCustomValidity("");
+        }
+        if (phoneInput.validity.valueMissing) {
+            phoneInput.setCustomValidity("Please enter your phone number");
+        }
+        else if (phoneInput.validity.patternMismatch) {
+            phoneInput.setCustomValidity("Please enter a valid phone number");
+        }
+        else {
+            phoneInput.setCustomValidity("");
+        }
+
+        // Validate password inputs
+        if (passwordInput.validity.valueMissing) {
+            passwordInput.setCustomValidity("Please enter your password");
+        }
+        else {
+            passwordInput.setCustomValidity("");
+        }
+        if (passwordConfirmationInput.validity.valueMissing) {
+            passwordConfirmationInput.setCustomValidity("Please confirm your password");
+        } 
+        else {
+            passwordConfirmationInput.setCustomValidity("");
+        }
+        if (passwordInput.value !== passwordConfirmationInput.value) {
+            passwordConfirmationInput.setCustomValidity("Passwords do not match");
+        }
+        else {
+            passwordConfirmationInput.setCustomValidity("");
+        }
+
+        // Submit the form to trigger validation error messages
+        form.reportValidity();
+    } 
+    else {
+        // Submit the form if all fields are valid
+        form.submit();
+    }
+});
