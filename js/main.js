6267200dc4402bfab0a8117d2a840cd45997bc06
stieludv <19951864+stieludv@users.noreply.github.com>
2023-04-21 02:05:05 +0200

    Realtime custom error messages in ::before element per input

--- js/main.js
+++ js/main.js
@@ -6,13 +6,20 @@ form.addEventListener("keyup", (e) => {
     }
 });
 
+form.addEventListener("mousedown", (e) => {
+    if (form.checkValidity() === false) {
+        validateInputs();
+    }
+});
+
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     if (form.checkValidity() === false) {
         validateInputs();
 
         // Submit the form to trigger validation error messages
-        form.reportValidity();
+        // form.reportValidity();
     } 
     else {
         // Submit the form if all fields are valid
@@ -32,54 +39,91 @@ function validateInputs() {
     const passwordInput = document.getElementById("password");
     const passwordConfirmationInput = document.getElementById("password-confirmation");
 
+    // Get .field-holder for CSS variables
+    const fieldHolder = document.querySelector(".field-holder");
+
+    // Error messages
+    const missingFirstNameError = "'Please enter your first name'";
+    const missingLastNameError = "'Please enter your last name'";
+    const missingEmailError = "'Please enter your email address'";
+    const invalidEmailError = "'Please enter a valid email address''";
+    const missingPhoneError = "'Please enter your phone number'";
+    const invalidPhoneError = "'Please enter a valid phone number'";
+    const missingPasswordError = "'Please enter your password'";
+    const missingPasswordConfirmationError = "'Please confirm your password'";
+    const passwordMismatchError = "'Passwords do not match'";
+
     // Validate name & lastname inputs
+    const firstNameInputFieldHolder = firstNameInput.parentElement;
+    const lastNameInputFieldHolder = lastNameInput.parentElement;
     if (firstNameInput.validity.valueMissing) {
-        firstNameInput.setCustomValidity("Please enter your first name");
+        firstNameInput.setCustomValidity(missingFirstNameError);
+        firstNameInputFieldHolder.style.setProperty("--first-name-error", missingFirstNameError);
     } else {
         firstNameInput.setCustomValidity("");
+        firstNameInputFieldHolder.style.setProperty("--first-name-error", "");
     }
     if (lastNameInput.validity.valueMissing) {
-        lastNameInput.setCustomValidity("Please enter your last name");
+        lastNameInput.setCustomValidity(missingLastNameError);
+        lastNameInputFieldHolder.style.setProperty("--last-name-error", missingLastNameError);
     }
     else {
         lastNameInput.setCustomValidity("");
+        lastNameInputFieldHolder.style.setProperty("--last-name-error", "");
     }
 
     // Validate email and phone inputs
+    const emailInputFieldHolder = emailInput.parentElement;
+    const phoneInputFieldHolder = phoneInput.parentElement;
     if (emailInput.validity.valueMissing) {
-        emailInput.setCustomValidity("Please enter your email address");
+        emailInput.setCustomValidity(missingEmailError);
+        emailInputFieldHolder.style.setProperty("--email-error", missingEmailError);
     } else if (emailInput.validity.typeMismatch) {
-        emailInput.setCustomValidity("Please enter a valid email address");
+        emailInput.setCustomValidity(invalidEmailError);
+        emailInputFieldHolder.style.setProperty("--email-error", invalidEmailError);
     } else {
         emailInput.setCustomValidity("");
+        emailInputFieldHolder.style.setProperty("--email-error", "");
     }
     if (phoneInput.validity.valueMissing) {
-        phoneInput.setCustomValidity("Please enter your phone number");
+        phoneInput.setCustomValidity(missingPhoneError);
+        phoneInputFieldHolder.style.setProperty("--phone-error", missingPhoneError);
     }
     else if (phoneInput.validity.patternMismatch) {
-        phoneInput.setCustomValidity("Please enter a valid phone number");
+        phoneInput.setCustomValidity(invalidPhoneError);
+        phoneInputFieldHolder.style.setProperty("--phone-error", invalidPhoneError);
     }
     else {
         phoneInput.setCustomValidity("");
+        phoneInputFieldHolder.style.setProperty("--phone-error", "");
     }
 
     // Validate password inputs
+    const passwordInputFieldHolder = passwordInput.parentElement;
+    const passwordConfirmationInputFieldHolder = passwordConfirmationInput.parentElement;
     if (passwordInput.validity.valueMissing) {
-        passwordInput.setCustomValidity("Please enter your password");
+        passwordInput.setCustomValidity(missingPasswordError);
+        passwordInputFieldHolder.style.setProperty("--password-error", missingPasswordError);
     }
     else {
         passwordInput.setCustomValidity("");
+        passwordInputFieldHolder.style.setProperty("--password-error", "");
     }
     if (passwordConfirmationInput.validity.valueMissing) {
-        passwordConfirmationInput.setCustomValidity("Please confirm your password");
+        passwordConfirmationInput.setCustomValidity(missingPasswordConfirmationError);
+        passwordConfirmationInputFieldHolder.style.setProperty("--password-confirmation-error", missingPasswordConfirmationError);
     } 
     else {
         passwordConfirmationInput.setCustomValidity("");
+        passwordConfirmationInputFieldHolder.style.setProperty("--password-confirmation-error", "");
     }
     if (passwordInput.value !== passwordConfirmationInput.value) {
-        passwordConfirmationInput.setCustomValidity("Passwords do not match");
+        passwordConfirmationInput.setCustomValidity(passwordMismatchError);
+        passwordConfirmationInputFieldHolder.style.setProperty("--password-confirmation-error", passwordMismatchError);
     }
     else {
         passwordConfirmationInput.setCustomValidity("");
+        passwordConfirmationInputFieldHolder.style.setProperty("--password-confirmation-error", "");
     }
 }
+
